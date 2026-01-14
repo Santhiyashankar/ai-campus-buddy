c18a3db7500e80063b744f8a30847ac32d93b7f3
Santhiyashankar <144871889+Santhiyashankar@users.noreply.github.com>
2026-01-14 19:05:46 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -15,13 +15,14 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* 🔥 FIX: redirect index.html */}
+        <Route path="/index.html" element={<Navigate to="/" replace />} />
+
         {/* Public */}
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-     
-      
-
 
         {/* Protected */}
         <Route
@@ -29,63 +30,17 @@ export default function App() {
           element={
             <ProtectedRoute>
               <Dashboard />
+            </ProtectedRoute>
+          }
+        />
 
-              
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/tasks"
-          element={
-            <ProtectedRoute>
-              <Tasks />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/collaboration"
-          element={
-            <ProtectedRoute>
-              <Collaboration />
-              
-              
-            </ProtectedRoute>
-          }
-          />
-          <Route
-          path="/resources"
-          element={
-            <ProtectedRoute>
-              <CampusResources />
-              
-              
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/gamification"
-          element={
-            <ProtectedRoute>
-              <Gamification />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile/>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/chat"
-          element={
-            <ProtectedRoute>
-              <ChatPage/>
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/tasks" element={<ProtectedRoute><Tasks /></ProtectedRoute>} />
+        <Route path="/collaboration" element={<ProtectedRoute><Collaboration /></ProtectedRoute>} />
+        <Route path="/resources" element={<ProtectedRoute><CampusResources /></ProtectedRoute>} />
+        <Route path="/gamification" element={<ProtectedRoute><Gamification /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/chat" element={<ProtectedRoute><ChatPage /></ProtectedRoute>} />
+
       </Routes>
     </BrowserRouter>
   );
