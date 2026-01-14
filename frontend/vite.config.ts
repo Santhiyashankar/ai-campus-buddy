8063782c7f23548f2757cf3d09651bbf3357bd2c
Santhiyashankar <144871889+Santhiyashankar@users.noreply.github.com>
2026-01-14 18:48:47 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
   base: './',
+  build: {
+    outDir: 'dist',       // make sure build output goes to 'dist'
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
 })
